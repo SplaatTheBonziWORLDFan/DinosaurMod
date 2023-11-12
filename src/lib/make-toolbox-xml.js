3815afeead6f50b6e674af22b0289239bfdfe021
Gabs <140965985+GabsTheCuriousKid@users.noreply.github.com>
2023-11-12 15:44:40 +0100

    hmmmmsee

--- src/lib/make-toolbox-xml.js
+++ src/lib/make-toolbox-xml.js
@@ -1510,15 +1510,16 @@ const menusCategory = function () {
         
         <block type="looks_costume"/>
         <block type="looks_backdrops"/>
-        
+        <block type="looks_getinput_menu"/>
         <block type="sound_sounds_menu"/>
-        
         <block type="event_touchingobjectmenu"/>
-        
+        <block type="event_broadcast_menu"/>
+        <block type="control_run_as_sprite_menu"/>
         <block type="control_create_clone_of_menu"/>
         
         <block type="sensing_touchingobjectmenu"/>
         <block type="sensing_keyoptions"/>
+        <block type="sensing_scrolldirections"/>
         ${categorySeparator}
     </category>
     `;
