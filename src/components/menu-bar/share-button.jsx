69db4f9b343e4956059086c023413fe179367380
Gabs <140965985+GabsTheCuriousKid@users.noreply.github.com>
2023-12-09 14:47:37 +0100

    i hope this works

--- src/components/menu-bar/share-button.jsx
+++ src/components/menu-bar/share-button.jsx
@@ -45,7 +45,8 @@ class ShareButton extends React.Component {
             'onUploadProject'
         ]);
         this.state = {
-            loading: false
+            loading: false,
+            imageUri: ''
         };
     }
     componentDidMount() {
@@ -72,7 +73,7 @@ class ShareButton extends React.Component {
             return;
         }
 
-        const imageUri = await getProjectThumbnail();
+        const imageUri = this.state.imageUri;
         e.source.postMessage({
             p4: {
                 type: 'image',
@@ -93,8 +94,28 @@ class ShareButton extends React.Component {
             }
         }, e.origin);
     }
-    onUploadProject() {
+    async onUploadProject() {
         if (this.state.loading) return;
+        if (!window.vm) return;
+        if (!window.vm.runtime) return;
+        if (!window.vm.renderer) return;
+
+        // get the project thumbnail
+        await new Promise((resolve) => {
+            getProjectThumbnail().then(dataUrl => {
+                this.setState({
+                    imageUri: dataUrl
+                });
+                resolve();
+            });
+            window.vm.renderer.draw(); // force the callback to run
+            setTimeout(() => {
+                window.vm.renderer.draw(); // force the callback to run
+            }, 50);
+            setTimeout(() => {
+                window.vm.renderer.draw(); // force the callback to run
+            }, 100);
+        });
 
         this.setState({
             loading: true
@@ -134,12 +155,12 @@ class ShareButton extends React.Component {
                 <div className={classNames(styles.shareContent)}>
                     {window.location.hash.includes('#') ?
                         <FormattedMessage
-                            defaultMessage="Remix To PM"
+                            defaultMessage="Remix"
                             description="Menu bar item for remixing"
                             id="gui.menuBar.remix"
                         /> :
                         <FormattedMessage
-                            defaultMessage="Upload To PM"
+                            defaultMessage="Upload to PM"
                             description="Label for project share button"
                             id="gui.menuBar.pmshare"
                         />}
