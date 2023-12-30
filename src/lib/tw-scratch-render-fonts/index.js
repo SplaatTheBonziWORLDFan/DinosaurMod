00ee9073be1871b43d60447e5ce44950032b4a5b
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2023-12-30 12:36:13 +0100

    i did my research and adding fonts here is only thing to do?

--- src/lib/tw-scratch-render-fonts/index.js
+++ src/lib/tw-scratch-render-fonts/index.js
@@ -37,7 +37,7 @@ const fontSource = {
     'Arcade': Arcade,
     'Archivo': Archivo,
     'Archivo Black': ArchivoBlack,
-    'Fredoka': Fredoka,
+    'Fredoka One': Fredoka
 };
 
 const fontData = {};
