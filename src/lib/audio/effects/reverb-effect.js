141189258f731cf199c052616ef082a0cbed54f8
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2024-08-06 16:15:13 +0200

    test

--- src/lib/audio/effects/reverb-effect.js
+++ src/lib/audio/effects/reverb-effect.js
@@ -12,10 +12,20 @@ class ReverbEffect {
         this.gain.gain.setValueAtTime(0.75, startTime);
         this.gain.gain.setValueAtTime(0, endTime);
 
-        this.convolver = this.audioContext.createConvolver();
+        this.delay1 = this.audioContext.createDelay();
+        this.delay2 = this.audioContext.createDelay();
+        this.feedback = this.audioContext.createGain();
+
+        this.delay1.delayTime.value = 0.1;
+        this.delay2.delayTime.value = 0.2;
+        this.feedback.gain.value = 0.5;
 
         this.input.connect(this.gain);
-        this.gain.connect(this.convolver);
+        this.gain.connect(this.delay1);
+        this.delay1.connect(this.feedback);
+        this.feedback.connect(this.delay1);
+        this.delay1.connect(this.delay2);
+        this.delay2.connect(this.output);
         this.input.connect(this.output);
     }
 }
