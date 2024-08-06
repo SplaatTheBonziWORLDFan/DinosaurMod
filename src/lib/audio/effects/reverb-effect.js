74aa23e8aa5e64180028d076314dcaf3c4275f7a
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2024-08-06 16:20:48 +0200

    it works but not a reverb effect

--- src/lib/audio/effects/reverb-effect.js
+++ src/lib/audio/effects/reverb-effect.js
@@ -14,19 +14,36 @@ class ReverbEffect {
 
         this.delay1 = this.audioContext.createDelay();
         this.delay2 = this.audioContext.createDelay();
-        this.feedback = this.audioContext.createGain();
+        this.delay3 = this.audioContext.createDelay();
+        this.feedback1 = this.audioContext.createGain();
+        this.feedback2 = this.audioContext.createGain();
+        this.feedback3 = this.audioContext.createGain();
 
         this.delay1.delayTime.value = 0.1;
         this.delay2.delayTime.value = 0.2;
-        this.feedback.gain.value = 0.5;
+        this.delay3.delayTime.value = 0.3;
+        this.feedback1.gain.value = 0.5;
+        this.feedback2.gain.value = 0.3;
+        this.feedback3.gain.value = 0.2;
 
         this.input.connect(this.gain);
         this.gain.connect(this.delay1);
-        this.delay1.connect(this.feedback);
-        this.feedback.connect(this.delay1);
-        this.delay1.connect(this.delay2);
+        this.gain.connect(this.delay2);
+        this.gain.connect(this.delay3);
+
+        this.delay1.connect(this.feedback1);
+        this.feedback1.connect(this.delay1);
+        this.delay1.connect(this.output);
+
+        this.delay2.connect(this.feedback2);
+        this.feedback2.connect(this.delay2);
+
+        this.delay3.connect(this.feedback3);
+        this.feedback3.connect(this.delay3);
+        
+        this.delay1.connect(this.output);
         this.delay2.connect(this.output);
-        this.input.connect(this.output);
+        this.delay3.connect(this.output);
     }
 }
 
