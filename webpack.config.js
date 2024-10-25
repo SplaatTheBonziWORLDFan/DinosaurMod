04655937c6f06cb92b17a49f63e7709a24da3639
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2024-10-26 01:25:04 +0200

    really?

--- webpack.config.js
+++ webpack.config.js
@@ -242,6 +242,11 @@ module.exports = [
             new TWGenerateServiceWorkerPlugin()
         ])
     })
+    resolve: {
+        alias: {
+          '@src': path.resolve(__dirname, 'src')
+        }
+    }
 ].concat(
     process.env.NODE_ENV === 'production' || process.env.BUILD_MODE === 'dist' ? (
         // export as library
